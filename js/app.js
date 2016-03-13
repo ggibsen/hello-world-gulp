ea91a23c38a2a116bc6f155cc9edb8dbe4fbc7f6
ggibsen <ggibsen@users.noreply.github.com>
2016-03-12 20:42:09 -0800

    Add angular controller and update comments

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 // Angular controller based app
+// Create the testApp module
+var testApp = angular.module('testApp', []);
+
+// define the controller and it's constructor function here, to avoid
+// having the function be apart of global scope
+testApp.controller('TestController', ['$scope', function($scope) {
+	$scope.testProp = "It worked!";
+}]);
+
+// now the $scope variable's testProp property can be used in a template's ng-controller directive
+// as it's bound to data, e.g. the string "It worked"
